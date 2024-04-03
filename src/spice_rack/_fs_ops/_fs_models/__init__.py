ad5fb78fd423c5545949d26510ec143e99d61268
Tim Robin <timo.a.robin@gmail.com>
2024-04-03 12:45:37 -0400

    fix import

--- src/spice_rack/_fs_ops/_fs_models/__init__.py
+++ src/spice_rack/_fs_ops/_fs_models/__init__.py
@@ -4,3 +4,9 @@ from spice_rack._fs_ops._fs_models._dir import *
 from spice_rack._fs_ops._fs_models._deferred import *
 from spice_rack._fs_ops._fs_models._constraints import *
 from spice_rack._fs_ops._fs_models._types import *
+
+from spice_rack._fs_ops._fs_models import (
+    _constraints as constraints,
+    _types as types,
+    _deferred as deferred
+)
--- tests/fs_ops/fs_models/test_constraints.py
+++ tests/fs_ops/fs_models/test_constraints.py
@@ -60,7 +60,7 @@ def text_setup_func(work_dir) -> t.Callable[[str], fs_ops.FilePath]:
 def test_file_ext_constraint_val(json_setup_func, text_setup_func):
     @pydantic.validate_call
     def _json_only(
-            fp: t.Annotated[fs_ops.FilePath, fs_ops.constraints.FileExtConstraint("json")]
+            fp: t.Annotated[fs_ops.FilePath, fs_ops.fs_models.FileExtConstraint("json")]
     ) -> t.Dict:
         with fp.open("rb") as _f:
             data = json.load(_f)
